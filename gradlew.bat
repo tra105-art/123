9e380615ecac5a1daae4f2298510c4f9f9a77616
tra105-art <debasispatra105@gmail.com>
2026-09-10 00:52:57 +0530

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,8 +0,0 @@
-@rem Gradle startup script for Windows
-@if "%DEBUG%"=="" @echo off
-setlocal
-set DIRNAME=%~dp0
-if "%DIRNAME%"=="" set DIRNAME=.
-set APP_HOME=%DIRNAME%
-set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
-java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
